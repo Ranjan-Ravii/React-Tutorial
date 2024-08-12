659b4d23c929390716c6a9dc261519938d277026
Ravi Ranjan <inboxraviiranjan@gmail.com>
2024-08-12 16:36:57 +0530

    Fix line endings

---
+++ lecture 1/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <h1>chai aur react | hello this is the first lesson of React.
+      Here i got to learn basics of React like how it was created to solve the specific problem. it was aimed to solve the some bugs in facebook through this library, obvsly it was not like what now it is, time to time it get changed. Since it is an open source library a large no of contributers contributes in this library but managed by the Meta.
+      
+      Secondly how the react files are created and managed. also learnt the about internal files that is created autometically when npm and utelity are installed.
+    </h1>
+  );
+}
+
+export default App;
