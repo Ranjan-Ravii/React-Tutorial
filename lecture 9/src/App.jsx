0747468c1b82391dcafc11ffa7e78b70d20ad9b9
Ravi Ranjan <inboxraviiranjan@gmail.com>
2024-09-01 11:42:20 +0530

    u

---
+++ lecture 9/src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [color, setColor] = useState("purple");
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
+
+  return (
+    <>
+      <div className='w-full h-screen duration-200'>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-l'>
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{backgroundColor: "red"}}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{backgroundColor: "green"}}
+            >
+              Green
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{backgroundColor: "blue"}}
+            >
+              Blue
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ lecture 9/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './pages/**/*.{html,js}',
+  './components/**/*.{html,js}',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
